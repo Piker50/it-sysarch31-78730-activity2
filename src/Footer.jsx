de428fa0e514b97aa91cf8eecc39d62279817288
Lanz Chrischar Silva <78730-13243423@SERVER2.DOMAIN>
2024-03-02 13:06:15 +0800

    Adding files

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getUTCFullYear()} Mugi.com</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <header>
+            <h1>Lanz Chrischar Silva</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
